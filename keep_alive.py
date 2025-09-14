15959f73ea8f5394804fc8adb69898d7888397e4
eudp-admin <infoeudp2017@gmail.com>
2025-09-14 10:31:19 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from threading import Thread
+import os
 
 app = Flask('')
 
 @app.route('/')
 def home():
-    return "EALPA Bot is alive!"
+    return "EALPA Bot is alive and running!"
 
 def run():
-  app.run(host='0.0.0.0', port=8080)
+  # Render የ PORT ተለዋዋጭን በራሱ ጊዜ ይሰጣል
+  port = int(os.environ.get('PORT', 8080))
+  app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
